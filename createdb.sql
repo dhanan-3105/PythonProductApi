239904794c0ee6d7030b861ce165b3f5a963268b
DJay <40359449+dhanan77@users.noreply.github.com>
2019-12-04 09:06:48 +0530

    Create createdb.sql

---
+++ createdb.sql
@@ -0,0 +1,5 @@
+use products;
+create table products_master (prodid int, prodname varchar(100), proddesc varchar(500));
+insert into products_master values (1,'TAB-SAMSUNG','SAMSUNG TAB G13 SERIES');
+insert into products_master values (2,'SURFACE','MS SURFACE WIN 10 SERIES 2');
+insert into products_master values (3,'MAC_TAB','APPLE MAC SERIES PAD 1.0');
