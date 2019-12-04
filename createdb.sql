df043e1d9f67126392159554f5a36b8f01e07ebe
DJay <40359449+dhanan77@users.noreply.github.com>
2019-12-04 09:07:28 +0530

    Update createdb.sql

--- createdb.sql
+++ createdb.sql
@@ -1,5 +1,5 @@
 use products;
-create table products_master (prodid int, prodname varchar(100), proddesc varchar(500));
-insert into products_master values (1,'TAB-SAMSUNG','SAMSUNG TAB G13 SERIES');
-insert into products_master values (2,'SURFACE','MS SURFACE WIN 10 SERIES 2');
-insert into products_master values (3,'MAC_TAB','APPLE MAC SERIES PAD 1.0');
+create table product_master (prodid int, prodname varchar(100), proddesc varchar(500));
+insert into product_master values (1,'TAB-SAMSUNG','SAMSUNG TAB G13 SERIES');
+insert into product_master values (2,'SURFACE','MS SURFACE WIN 10 SERIES 2');
+insert into product_master values (3,'MAC_TAB','APPLE MAC SERIES PAD 1.0');
